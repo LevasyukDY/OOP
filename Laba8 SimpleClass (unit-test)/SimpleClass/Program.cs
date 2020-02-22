e78011a03a73626efe25a2d19180b0d6e98ff624
LevasyukDY <Levasyuk@gmail.com>
2020-02-22 16:41:32 +0900

    .

---
+++ Laba8 SimpleClass (unit-test)/SimpleClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex
+            {
+                Re = 5,
+                Im = 7
+            };
+
+            Complex b = new Complex
+            {
+                Re = 10,
+                Im = 4
+            };
+
+            Complex c = a + b;
+
+            Console.WriteLine($"Operation: ({a.Re} + i{a.Im}) + ({b.Re} + i{b.Im})");
+            Console.WriteLine($"Result: Re = {c.Re} Im = {c.Im}");
+            Console.WriteLine("Absolute Value: " + c.GetAbs());
+            Console.WriteLine("Argument: " + c.GetArg() + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
